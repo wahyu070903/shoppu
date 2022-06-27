dcbfd0e5a9f0e9b65ebe9b1a486fde94e412aa88
[wahyu dwi] <[wahyudwiahmadi793@gmail.com]>
2022-06-27 21:19:29 +0700

    firse commit

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,5 @@ use App\Http\Controllers\ProductsController;
 
 Route::get('/',[ProductsController::class, 'index']);
 Route::get('/product/{id}',[ProductsController::class, 'product']);
-
-Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-    Route::get('/',[ProductsController::class, 'index'])->name('home');
-    Route::get('/product/{id}',[ProductsController::class, 'product'])->name('detail');
-});
+Route::get('/',[ProductsController::class, 'index'])->name('home');
+Route::get('/product/{id}',[ProductsController::class, 'product'])->name('detail');
